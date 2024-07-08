f3363278bad56797d53207858ff0e20d6cd91d31
Legend1381 <mostafa.nazari9494@gmail.com>
2024-07-08 23:46:07 +0330

    add the hero component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <main-nav />
+  <the-hero />
 </template>
 
 <script>
 import MainNav from '@/components/MainNav.vue'
+import TheHero from '@/components/TheHero.vue'
 export default {
   name: 'App',
   components: {
-    MainNav
+    MainNav,
+    TheHero
   }
 }
 </script>
---
+++ src/components/TheHero.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <section>I can be your hero baby</section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'TheHero'
+}
+</script>
