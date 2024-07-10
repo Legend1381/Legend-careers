b6d16abec48e5be73319657887aeac2e9810323d
Legend1381 <mostafa.nazari9494@gmail.com>
2024-07-10 16:39:14 +0330

    add grid to the hero

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,7 +1,15 @@
 <template>
   <main>
     <section class="flex flex-col h-screen bg-yellow-200 pt-10 pb-20">
-      I can be your hero baby
+      <div class="grid grid-cols-12">
+        <div class="col-span-1 col-start-1">1 column</div>
+
+        <div class="col-span-5 col-start-2">1 column</div>
+
+        <div class="col-span-5 col-start-7">1 column</div>
+
+        <div class="col-span-1 col-start-12">1 column</div>
+      </div>
     </section>
   </main>
 </template>
