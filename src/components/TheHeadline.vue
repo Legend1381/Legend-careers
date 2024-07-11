72d4d6a585f9196416dcdf0f3b1ba2b6ff99ac9d
Legend1381 <mostafa.nazari9494@gmail.com>
2024-07-11 23:21:31 +0330

    add interval to the head line

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <h1>build for every one</h1>
+    <h1>{{ action }} for every one</h1>
     <h2>find your next job at Legend corp.</h2>
   </section>
 </template>
@@ -9,11 +9,26 @@
 export default {
   data() {
     return {
-      sample: 'Hello!'
+      action: 'Build',
+      interval: null
     }
   },
   created() {
-    console.log('hey!', this.sample)
+    this.changeTitle()
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ['Build', 'Create', 'Design', 'Code']
+        const currentActionIndex = actions.indexOf(this.action)
+        const nextActionIndex = (currentActionIndex + 1) % 4
+        const nextAction = actions[nextActionIndex]
+        this.action = nextAction
+      }, 3000)
+    }
   }
 }
 </script>
