524538976fd7e3d14fa083f67c2b49d1056b7c0d
Legend1381 <mostafa.nazari9494@gmail.com>
2024-07-13 23:27:42 +0330

    refactor actionclasses function in head line

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -19,10 +19,7 @@ export default {
   computed: {
     actionClasses() {
       return {
-        build: this.action === 'Build',
-        create: this.action === 'Create',
-        design: this.action === 'Design',
-        code: this.action === 'Code'
+        [this.action.toLowerCase()]: true
       }
     }
   },
